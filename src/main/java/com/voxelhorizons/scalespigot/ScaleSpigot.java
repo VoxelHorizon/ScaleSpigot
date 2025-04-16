c1cab1431f0f1f71c60a9ea688342cd88ba12769
Dan Alexander <71574715+Fibbicles@users.noreply.github.com>
2025-04-16 17:06:42 +1200

    Second Fix Attempt

--- src/main/java/com/voxelhorizons/scalespigot/ScaleSpigot.java
+++ src/main/java/com/voxelhorizons/scalespigot/ScaleSpigot.java
@@ -2,7 +2,7 @@ package com.voxelhorizons.scalespigot;
 
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
-import com.littleadventures.shaded.jedis.Jedis;
+import redis.clients.jedis.Jedis;
 
 public class ScaleSpigot extends JavaPlugin {
     private Jedis redis;
